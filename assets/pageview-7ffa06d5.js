bfc05b38c312ca07fe3880a985cf8d44eaba4ac0
wuiyan <126488465+wuiyan@users.noreply.github.com>
2023-08-18 11:42:37 +0000

    Deploying to gh-pages from @ wuiyan/wuiyan.github.io@2d7df9d590dfa6e3caa8b62bc0b566f3de997689 🚀

---
+++ assets/pageview-7ffa06d5.js
@@ -0,0 +1,1 @@
+import{i as e}from"./app-1b738a4f.js";import"./framework-646c68e2.js";const a=()=>e({serverURL:"https://comment.yils.blog/"});export{a as updatePageview};
--- assets/pageview-97db254d.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./app-ca2bde97.js";import"./framework-646c68e2.js";const a=()=>e({serverURL:"https://comment.yils.blog/"});export{a as updatePageview};
